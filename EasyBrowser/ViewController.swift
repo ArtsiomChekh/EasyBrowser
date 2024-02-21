c459ceb806fa2cc64860cc71381de2a76525cfbc
Artsiom Chekh <qbartem@gmail.com>
2024-02-21 17:53:24 +0300

    first commit

---
+++ EasyBrowser/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  EasyBrowser
+//
+//  Created by Artsiom Chekh on 21.02.24.
+//
+
+import UIKit
+import WebKit
+
+class ViewController: UIViewController, WKNavigationDelegate {
+    var webView: WKWebView!
+    
+    override func loadView() {
+        webView = WKWebView()
+        webView.navigationDelegate = self
+        view = webView
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let url = URL(string: "https://www.apple.com")!
+        webView.load(URLRequest(url: url))
+        webView.allowsBackForwardNavigationGestures = true
+    }
+
+
+}
+
